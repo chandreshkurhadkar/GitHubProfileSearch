14d61ad8ec66d25e23faaac085bcdbda5790ec78
Chandresh <52243230+chandreshkurhadkar@users.noreply.github.com>
2019-08-07 10:14:19 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyserviceService } from '../myservice-service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
 })
-export class AppComponent {
-  title = 'Project';
+export class Component1Component implements OnInit {
+  users: any;
+  repos: any;
+  username: string;
+  constructor(private service: MyserviceService) {
+    this.users = false;
+   }
+   searchUser() {
+     this.service.updateUser(this.username);
+
+     this.service.getUser().subscribe(user => {
+      this.users = user;
+      console.log( "user" , user)
+    });
+     this.service.getRepository().subscribe(repos=>{
+      // console.log(repos);
+      this.repos = repos;
+    });
+   }
+  ngOnInit() {
+  }
+
 }
